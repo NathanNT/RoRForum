deb68df8c60b6cf42c7b604743b479a7e33b973d
Nathan <nathancpt1@gmail.com>
2019-11-03 20:43:54 +0100

    Updates for heroku deployment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :gossip, optional: true
-  belongs_to :user, optional: true
+  belongs_to :user
+    validates :user_id, presence: true	
+  validates :gossip_id, presence: true
 end
